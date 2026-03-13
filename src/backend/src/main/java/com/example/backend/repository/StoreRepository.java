40064c88d3770cb6577d83a42e53d8a0fbe772c7
Nirmalie Perera <nirmalieshehani27@gmail.com>
2026-03-13 19:19:21 +0530

    Remove deprecated store module files

--- src/backend/src/main/java/com/example/backend/repository/StoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-    boolean existsByStoreId(String storeId);
-}
